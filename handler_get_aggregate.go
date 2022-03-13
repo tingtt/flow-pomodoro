751ed21338516addb91223bd6bb78d044c504e06
tingtt <ting.taku@gmail.com>
2022-03-13 20:50:03 +0900

    [add] get aggregated pomodoro

---
+++ handler_get_aggregate.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flow-pomodoros/jwt"
+	"flow-pomodoros/pomodoro"
+	"net/http"
+	"time"
+
+	jwtGo "github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+)
+
+type GetAggregatedQueryParam struct {
+	ProjectId          *uint64   `query:"project_id" validate:"omitempty"`
+	IncludeSubProjects bool      `query:"include_sub_project" validate:"omitempty"`
+	Start              time.Time `query:"start" validate:"required"`
+	End                time.Time `query:"end" validate:"required"`
+}
+
+func GetAggregated(c echo.Context) error {
+	// Check token
+	u := c.Get("user").(*jwtGo.Token)
+	userId, err := jwt.CheckToken(*jwtIssuer, u)
+	if err != nil {
+		c.Logger().Debug(err)
+		return c.JSONPretty(http.StatusNotFound, map[string]string{"message": err.Error()}, "	")
+	}
+
+	// Bind query
+	q := new(GetAggregatedQueryParam)
+	if err = c.Bind(q); err != nil {
+		// 400: Bad request
+		c.Logger().Debug(err)
+		return c.JSONPretty(http.StatusBadRequest, map[string]string{"message": err.Error()}, "	")
+	}
+
+	// Validate query
+	if err = c.Validate(q); err != nil {
+		// 422: Unprocessable entity
+		c.Logger().Debug(err)
+		return c.JSONPretty(http.StatusUnprocessableEntity, map[string]string{"message": err.Error()}, "	")
+	}
+	if q.Start.After(q.End) {
+		// 422: Unprocessable entity
+		c.Logger().Debug("`start` must before `end`")
+		return c.JSONPretty(http.StatusUnprocessableEntity, map[string]string{"message": "`start` must before `end`"}, "	")
+	}
+
+	// Get aggregatedPomodoro
+	aggregatedPomodoro, err := pomodoro.GetAggregated(userId, q.Start, q.End, q.ProjectId, q.IncludeSubProjects)
+	if err != nil {
+		// 500: Internal server error
+		c.Logger().Debug(err)
+		return c.JSONPretty(http.StatusInternalServerError, map[string]string{"message": err.Error()}, "	")
+	}
+
+	return c.JSONPretty(http.StatusOK, aggregatedPomodoro, "	")
+}
--- pomodoro/aggregate.go
+++ pomodoro/aggregate.go
@@ -11,3 +11,47 @@ type Time struct {
 	Time      uint   `json:"time"`
 	ProjectId uint64 `json:"project_id"`
 }
+
+func GetAggregated(userId uint64, start time.Time, end time.Time, projectId *uint64, includeSubProject bool) (a AggregatedPomodoro, err error) {
+	var pomodoros []Pomodoro
+
+	if projectId != nil {
+		pomodoros, err = GetListProjectId(userId, start, end, *projectId, includeSubProject)
+		if err != nil {
+			return
+		}
+	} else {
+		pomodoros, err = GetList(userId, start, end)
+		if err != nil {
+			return
+		}
+	}
+
+	var projects map[uint64]uint = map[uint64]uint{}
+
+	for _, pomodoro := range pomodoros {
+		if pomodoro.End == nil {
+			continue
+		}
+
+		// Trim overdue
+		if pomodoro.Start.Before(start) {
+			pomodoro.Start = start
+		}
+		if pomodoro.End.After(end) {
+			pomodoro.End = &end
+		}
+
+		projects[pomodoro.ProjectId] += uint(pomodoro.End.Sub(pomodoro.Start).Seconds())
+	}
+
+	var timeAry []Time
+
+	for k, v := range projects {
+		timeAry = append(timeAry, Time{v, k})
+	}
+
+	a.BaseTime = start
+	a.Data = timeAry
+	return
+}
