dd54eac798c268e5c5695aed4814bb18d3539246
tingtt <ting.taku@gmail.com>
2022-06-07 12:11:50 +0900

    [clean] optimize row scan

--- pomodoro/get_last.go
+++ pomodoro/get_last.go
@@ -25,37 +25,15 @@ func GetLast(userId uint64) (p Pomodoro, notFound bool, err error) {
 	}
 	defer rows.Close()
 
-	var (
-		id              uint64
-		start           time.Time
-		end             sql.NullTime
-		todoId          uint64
-		projectId       sql.NullInt64
-		parentProjectId sql.NullInt64
-	)
 	if !rows.Next() {
 		// Not found
 		notFound = true
 		return
 	}
-	err = rows.Scan(&id, &start, &end, &todoId, &projectId, &parentProjectId)
+	p = Pomodoro{}
+	err = rows.Scan(&p.Id, &p.Start, &p.End, &p.TodoId, &p.ProjectId, &p.ParentProjectId)
 	if err != nil {
-		return Pomodoro{}, false, err
-	}
-
-	p.Id = id
-	p.Start = start
-	if end.Valid {
-		p.End = &end.Time
-	}
-	p.TodoId = todoId
-	if projectId.Valid {
-		projectIdTmp := uint64(projectId.Int64)
-		p.ProjectId = &projectIdTmp
-	}
-	if parentProjectId.Valid {
-		parentProjectIdTmp := uint64(parentProjectId.Int64)
-		p.ParentProjectId = &parentProjectIdTmp
+		return
 	}
 
 	return
